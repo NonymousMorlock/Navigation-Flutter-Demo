7b16b51634c68de80c0db974844982f2890291f7
NonymousMorlock <59588180+NonymousMorlock@users.noreply.github.com>
2022-05-19 21:48:07 +0000

    navigation-flutter-demo

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'screen1.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Screen1(),
+    );
+  }
+}
---
+++ lib/screen2.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Screen2 extends StatelessWidget {
+  const Screen2({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: const Text('Screen 2'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          color: Colors.blue,
+          child: const Text('Go Back To Screen 1'),
+          onPressed: () {},
+        ),
+      ),
+    );
+  }
+}
